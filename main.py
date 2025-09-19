7224121fd4d74151d31b8c80afe9b20419beb24a
shekofaamiri <shekofaamiri1@gmail.com>
2025-09-19 22:50:58 +0330

    Add if for being qaeam alzavia

--- main.py
+++ main.py
@@ -14,5 +14,7 @@ if x==y or y==z or z==x:
     if x!=y or y!=z or z!=x:
         
         print("mokhtalive azla.")
+    if x **2 = y **2+z **2 or y **2=x **2 +z**2 or z**2 =x**2+y**2:
+        print("qaeam alzavia.")   
         
         
